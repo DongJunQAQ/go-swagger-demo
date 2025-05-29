0ee2618c5ef300d8b21f0e6d9f3778b7c0d653ac
DongJun <you@example.com>
2025-05-29 15:05:04 +0800

    style(web): 增加注释

--- main.go
+++ main.go
@@ -14,16 +14,16 @@ var users []*api.User
 
 func main() {
 	r := gin.Default()
-	r.Use(cors.New(cors.Config{ //全局中间件，每个请求都会先经过这里，默认允许的请求方法为GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS
-		AllowHeaders:     []string{"authorization", "content-type"}, //指定哪些请求头可以在跨域请求中使用
-		AllowCredentials: true,                                      //是否允许跨源请求带有凭证（如cookies）
-		AllowOriginFunc: func(origin string) bool { //动态决定是否允许特定的源访问资源，origin是发起请求的URL
-			if strings.HasPrefix(origin, "http://localhost") { //开发环境，检查请求的源是否以http://localhost开头
-				return true //允许该请求源
+	r.Use(cors.New(cors.Config{
+		AllowHeaders:     []string{"authorization", "content-type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
 			}
-			return strings.Contains(origin, "yourDomain.com") //生产环境，检查请求源是否包含生产环境下的域名
+			return strings.Contains(origin, "yourDomain.com")
 		},
-		MaxAge: 12 * time.Hour, //preflight请求的缓存时间，使得浏览器在12小时内不需要重新发送preflight请求，而是可以使用缓存的结果来决定是否允许实际请求
+		MaxAge: 12 * time.Hour,
 	}))
 	r.POST("/user", Create)
 	r.GET("/user/:name", Get) //路径参数路由
@@ -33,14 +33,14 @@ func main() {
 	}
 }
 
-func Create(c *gin.Context) {
-	var newUser api.User //新添加的用户
-	if err := c.ShouldBind(&newUser); err != nil {
+func Create(c *gin.Context) { //创建用户
+	var newUser api.User                           //新添加的用户
+	if err := c.ShouldBind(&newUser); err != nil { //ShouldBind:根据请求的Content-Type（如 application/json），自动将请求体中的数据解析到newUser结构体中
 		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error(), "code": 10001})
 		return
 	}
 	for _, u := range users {
-		if u.Name == newUser.Name {
+		if u.Name == newUser.Name { //检查新添加的用户是否已存在
 			c.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("用户%s已存在", newUser.Name), "code": 10002})
 			return
 		}
@@ -49,7 +49,7 @@ func Create(c *gin.Context) {
 	c.JSON(http.StatusOK, newUser)
 }
 
-func Get(c *gin.Context) {
+func Get(c *gin.Context) { //查询用户
 	username := c.Param("name") //提取路径参数
 	for _, u := range users {
 		if u.Name == username {
