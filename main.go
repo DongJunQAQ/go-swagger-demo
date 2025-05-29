1b83fb61b69f72c5fbcfc23d25fa638aee91bf57
DongJun <you@example.com>
2025-05-29 14:49:56 +0800

    feat(web): 添加跨域功能

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go-swagger-demo/api"
 	"net/http"
+	"strings"
+	"time"
 )
 
 var users []*api.User
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{ //全局中间件，每个请求都会先经过这里，默认允许的请求方法为GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS
+		AllowHeaders:     []string{"authorization", "content-type"}, //指定哪些请求头可以在跨域请求中使用
+		AllowCredentials: true,                                      //是否允许跨源请求带有凭证（如cookies）
+		AllowOriginFunc: func(origin string) bool { //动态决定是否允许特定的源访问资源，origin是发起请求的URL
+			if strings.HasPrefix(origin, "http://localhost") { //开发环境，检查请求的源是否以http://localhost开头
+				return true //允许该请求源
+			}
+			return strings.Contains(origin, "yourDomain.com") //生产环境，检查请求源是否包含生产环境下的域名
+		},
+		MaxAge: 12 * time.Hour, //preflight请求的缓存时间，使得浏览器在12小时内不需要重新发送preflight请求，而是可以使用缓存的结果来决定是否允许实际请求
+	}))
 	r.POST("/user", Create)
 	r.GET("/user/:name", Get) //路径参数路由
 	err := r.Run(":8080")
